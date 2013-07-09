9581647b61efb2760c3d94a2ec6fc571203f9c29
TennisIDCTO <arun@tennisbook.com.au>
2013-07-09 17:34:00 +0000

    Grader.js part3

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+//revision for assignment
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -42,6 +44,23 @@ var cheerioHtmlFile = function(htmlfile) {
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
+};
+
+//revision for assignment
+var parseURL = function(infile) {
+    var instr = infile.toString();
+    return instr;
+};
+
+var checkURL = function(url, checksfile) {
+    $ = cheerio.load(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
@@ -64,11 +83,22 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists)) //removed HTMLFILE_DEFAULT as switch in this case is less intuitive
+		//revision for assignment
+		.option('-u, --url <url>', 'Custom URL', clone(parseURL))
         .parse(process.argv);
+	if (program.file) {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+	}
+	if (program.url) {
+	rest.get(program.url).on('complete', function(result, response) {
+	    var checkJson = checkURL(result, program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	});
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
