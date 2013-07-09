6219d9c9e50e0a0c046795fae1295e07cb16119d
TennisIDCTO <arun@tennisbook.com.au>
2013-07-09 13:47:28 +0000

    New web.js

--- web.js
+++ web.js
@@ -1,12 +1,6 @@
-var express = require('express');
+var express = require('express'),app = express.createServer(express.logger()),fs = require('fs');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+app.get('/', function(request, response) { response.send(fs.readFileSync("./index.html", "utf8").toString()); });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function() { console.log("Listening on " + port); });
